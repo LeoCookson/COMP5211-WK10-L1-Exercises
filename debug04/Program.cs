7e0587cd0aa887844e7831049cc5772b124edc9e
Jeffrey Kranenburg <jeffrey.kranenburg@g.toiohomai.ac.nz>
2018-05-15 08:16:46 +1200

    Added debug4

---
+++ debug04/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace debug04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Get user input for Farenheit");
+
+            var input = int.Parse(Console.ReadLine());
+
+            var conversion = (input - 32) * 10 / 18;
+
+            Console.WriteLine($"The degrees in Celsius is {conversion}");
+        }
+    }
+}
